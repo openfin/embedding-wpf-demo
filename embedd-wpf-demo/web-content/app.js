1fd1c6a897b35e76f2d2617d715e6c25f59d9efc
Mateusz Magier <mateusz@openfin.co>
2017-01-31 17:04:00 +0000

    using fin-hypergrid.min.js instead of fin-hypergrid.js, changed schema for data headers

--- embedd-wpf-demo/web-content/app.js
+++ embedd-wpf-demo/web-content/app.js
@@ -1,30 +1,31 @@
 var headers = [
-    'last',
-    'first',
-    'pets',
-    'birth date',
-    'birth state',
-    'residence state',
-    'employed',
-    'income',
-    'travel'
+    { name: 'last', header: 'Last Name' },
+    { name: 'first', header: 'First Name' },
+    { name: 'pets', header: 'Pets' },
+    { name: 'birth date', header: 'Birth Date' },
+    { name: 'birth state', header: 'Birth State' },
+    { name: 'residence state', header: 'Residence State' },
+    { name: 'employed', header: 'Employed' },
+    { name: 'income', header: 'Income' },
+    { name: 'travel', header: 'Travel' }
 ];
+
 var grid = new fin.Hypergrid('#json-example', {
     data: [],
     schema: headers,
 });
 
 grid.addProperties({
-    showRowNumbers:false, 
-    noDataMessage: "", 
+    showRowNumbers: false,
+    noDataMessage: "",
     columnAutosizing: false,
-    showFilterRow:false
+    showFilterRow: false
 });
 
-fin.desktop.main(function (){
+fin.desktop.main(function () {
     fin.desktop.InterApplicationBus.subscribe("*",
         "user-data",
         function (message, uuid) {
             grid.behavior.setData(message.data);
-    });
+        });
 });
