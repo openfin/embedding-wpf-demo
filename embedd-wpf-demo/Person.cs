b2f8abbf2d09895d1048342ffd5a8923e3653f65
Mateusz Magier <mateusz@openfin.co>
2017-01-31 18:31:56 +0000

    limiting precision of displayed numbers to 2 decimal places

--- embedd-wpf-demo/Person.cs
+++ embedd-wpf-demo/Person.cs
@@ -7,6 +7,10 @@ namespace embedd_wpf_demo
 {
     class Person
     {
+        private const int Precision = 2;
+        private double _income;
+        private double _travel;
+
         public string First { get; set; }
         public string Last { get; set; }
         public int Pets { get; set; }
@@ -14,7 +18,15 @@ namespace embedd_wpf_demo
         public string ResidenceState { get; set; }
         public string BirthState { get; set; }
         public bool Employed { get; set; }
-        public double Income { get; set; }
-        public double Travel { get; set; }
+        public double Income
+        {
+            get { return Math.Round(_income, PRECISION); }
+            set { _income = value; }
+        }
+        public double Travel
+        {
+            get { return Math.Round(_travel, PRECISION); }
+            set { _travel = value; }
+        }
     }
 }
